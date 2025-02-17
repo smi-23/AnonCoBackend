20c158501d31bf9053f4329fba8450f63d55041e
smi-23 <latack789@naver.com>
2025-02-17 14:31:13 +0900

    Initial commit

---
+++ src/main/java/com/AnonCoBackend/AnonCoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.AnonCoBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonCoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonCoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AnonCoBackend/AnonCoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AnonCoBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonCoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
